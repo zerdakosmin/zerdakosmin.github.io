3456aef44c959f76e73c0720985a8dbc674d4857
zerdakosmin <37200853+zerdakosmin@users.noreply.github.com>
2018-09-25 21:25:43 -0700

    Update page layout
    Also create JS file

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// currently not in use
+
+function showCredit() {
+  document.getElementById("credit").style.visibility='visible';
+}
+
+function hideCredit() {
+  document.getElementById("credit").style.visibility='hidden';
+}
